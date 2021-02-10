5a10f0726c3f3d4b482b1b7cfdb7e73efe33a14e
jihye <smskjh95@gmail.com>
2021-02-10 16:54:37 +0900

    join_2.10

--- src/main/java/org/zerock/domain/JoinTeacherVO.java
+++ src/main/java/org/zerock/domain/JoinTeacherVO.java
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
 package org.zerock.domain;
 
 import lombok.Data;
 
 @Data
 public class JoinTeacherVO {
-
+	
+	// 선생님 테이블
 	private String userId;
 	private char atGrade;
 	private String className;
@@ -17,23 +17,3 @@ public class JoinTeacherVO {
 	private char userType;
 	
 }
-=======
-package org.zerock.domain;
-
-import lombok.Data;
-
-@Data
-public class JoinTeacherVO {
-
-	private String userId;
-	private char atGrade;
-	private String className;
-	private String userName;
-	private String userPw;
-	private String userPhone;
-	private String userAddr;
-	private String userEmail;
-	private char userType;
-	
-}
->>>>>>> branch 'develop' of https://github.com/KimJi-Hye/Project-SKY.git
